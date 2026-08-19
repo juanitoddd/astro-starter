143ef819c3830a1c38ce160177d75b896220b02b
juanddd <juand.diazd@gmail.com>
2026-08-20 00:40:52 +0200

    ADD Astro Pages

---
+++ src/constants/languages.ts
@@ -0,0 +1,4 @@
+// export const languages = ["en-US", "de-DE", "es-ES", "fr-FR", "it-IT"];
+export const languages = ["en-US", "de-DE"];
+
+export const languagePrefixes = languages.map((l) => l.split('-')[0]);
